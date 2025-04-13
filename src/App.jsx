fb6ee253d3bbdcb4a768670a81eef806a381c216
Carlos Junior <carlosjrdev23@gmail.com>
2025-04-13 17:25:35 -0300

    alteração do titulo do site

--- src/App.jsx
+++ src/App.jsx
@@ -72,7 +72,7 @@ function App() {
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
       <div className="w-[500px] space-y-4">
         <Title>
-          GERENCIADOR DE TAREFAS
+          AGENDA DE TAREFAS
         </Title>
 
         <AddTasks onAddTaskSubmit={onAddTaskSubmit} />
